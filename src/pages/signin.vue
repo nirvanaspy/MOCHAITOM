8d424d1a5593f4aa56556a5d2ef6effddd1d38ec
zhaoqi <1925654299@qq.com>
2018-01-25 18:49:34 +0800

    登录

--- src/pages/signin.vue
+++ src/pages/signin.vue
@@ -1,34 +1,33 @@
 <template>
-	<div>
-
-	    <div class="row-fluid login-wrapper">
-	        <a href="#" style="text-decoration:none">
-	            <!-- <img class="logo" src="img/logo-white.png" /> -->
+  <div :style="{backgroundImage: 'url(img/bgs/10.jpg)' }">
+      <div class="row-fluid login-wrapper">
+          <a href="#">
+              <!-- <img class="logo" src="img/logo-white.png" /> -->
               <h1 style="color:white;">一体化运维管理平台</h1>
-	        </a>
+          </a>
           <br/><br/>
 
-	        <div class="span4 box">
-	            <div class="content-wrap">
-	                <h6>登录</h6>
-	                <input id ="username" class="span12" type="text" placeholder="用户名" />
-	                <input id ="password" class="span12" type="password" placeholder="密码"/>
-                  <input id ="ip" class="span12" type="text" placeholder="请输入IP"/>
-	                <a href="#" class="forgot">忘记密码?</a>
-	                <div class="remember">
-	                    <input id="remember-me" type="checkbox" />
-	                    <label for="remember-me">记住我</label>
-	                </div>
-					<button class="btn-glow primary" style="width: 100px;" v-on:click="login">登录</button>
-	            </div>
-	        </div>
+          <div class="span4 box">
+              <div class="content-wrap">
+                  <h6>登录</h6>
+                  <input id ="username" class="span12" type="text" placeholder="用户名" />
+                  <input id ="password" class="span12" type="password" placeholder="密码"/>
+                  <input id ="ip" class="span12" type="text" placeholder="请输入IP" value="192.168.0.122"/>
+                  <a href="#" class="forgot">忘记密码?</a>
+                  <div class="remember">
+                      <input id="remember-me" type="checkbox" />
+                      <label for="remember-me">记住我</label>
+                  </div>
+          <button class="btn-glow primary" style="width: 100px;" v-on:click="login">登录</button>
+              </div>
+          </div>
 
-	        <div class="span4 no-account">
-	            <p>Don't have an account?</p>
-	            <a href="signup.html">Sign up</a>
-	        </div>
-	    </div>
-	</div>
+          <div class="span4 no-account">
+  
+              <a href="signup.html">注册</a>
+          </div>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -45,24 +44,18 @@ export default {
       var username = $("input#username").val();
       var password = $("input#password").val();
       var ip = $("input#ip").val();
-
-
       if (username.length == 0 || password.length == 0) {
-    		alert("请输入正确的用户名或密码。");
-
-    		return;
+        alert("请输入正确的用户名或密码。");
+        return;
       }
-
       //ip地址  
       var exp=/^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/;  
       var reg = ip.match(exp);  
       if(reg==null){  
         alert("IP地址不合法！"); 
-
         return; 
       }  
       
-
       this.$axios.post("users/login",{},
           {
             auth: {
@@ -73,7 +66,6 @@ export default {
         )
         .then(response => {
           this.$router.replace({ path: "/selectProject" });
-
           //将用户名、密码的值存入cookie中
           let expireDays = 1000 * 60 * 60 * 24 * 15;
           this.setCookie('username', username, expireDays);
@@ -94,7 +86,6 @@ export default {
   }
 };
 </script>
-
 <style>
 
 </style>
