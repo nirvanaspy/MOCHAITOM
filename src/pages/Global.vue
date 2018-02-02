b2655605febbbc474c5812c6372cc83fae2e324f
XYmar <xqingliuwu@163.com>
2018-02-02 13:20:13 +0800

    提交全局文件

---
+++ src/pages/Global.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  /* eslint-disable */
+
+  export default {
+    data() {
+      return {},
+        created()
+      {
+
+      }
+    }
+  }
+
+</script>
+
+<style>
+
+
+</style>
