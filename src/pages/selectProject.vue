68633f02876ba53cfdcc562fb86a639a51f8eec6
XYmar <xqingliuwu@163.com>
2018-02-05 15:37:43 +0800

    新版：选择工程页面

--- src/pages/selectProject.vue
+++ src/pages/selectProject.vue
@@ -177,7 +177,7 @@
       var projectId = this.getCookie('projectId');
       var username = this.getCookie('username');
       var password = this.getCookie('password');
-      this.$axios.get(this.getIP() + 'project/', {
+      this.$axios.get(this.getIP() + 'projects', {
         //设置头
         headers: {
           'content-type': 'application/x-www-form-urlencoded'
