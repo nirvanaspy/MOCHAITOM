ec57a79a7bdd0883981af7c07e5a570dc649a903
XYmar <xqingliuwu@163.com>
2018-02-10 17:04:35 +0800

    新版：基线页面： 1、静态页面 2、基线的查询功能及显示

--- src/pages/baseline.vue
+++ src/pages/baseline.vue
@@ -24,13 +24,6 @@
               <th class="span4 sortable">
                 名称
               </th>
-              <th class="span3 sortable">
-                <span class="line"></span>版本
-              </th>
-
-              <th class="span3">
-                <span class="line"></span>组件
-              </th>
 
               <th class="span3">
                 <span class="line"></span>描述
@@ -43,39 +36,16 @@
             </thead>
             <tbody>
             <!-- row -->
-            <tr class="first" v-for="baseline in baselinesA" :id="baseline.id">
-              <td style="display:none">{{baseline.id}}</td>
-              <td>{{baseline.name}}</td>
-              <td>{{baseline.version}}</td>
-              <td>
-
-                    <!--<input type="button" class="btn-flat gray" value="组件详情" @mouseenter="enter($event)"/>-->
-                    <popper trigger="click" :options="{placement: 'top'}">
-                      <div class="popper">
-                        <!--Popper Content-->
-                        <div id="todo-list-example" class="addli" style="height: 200px;width: 200px;">
-                          <ul class="addul">
-                            <li v-for="(comp, index) in comps" :key="index">
-                              {{comp.name}}
-                            </li>
-                          </ul>
-                        </div>
-
-                      </div>
-
-                      <input type="button" class="btn-flat gray" value="组件详情" slot="reference" @click="enter($event)"/>
-
-                    </popper>
-
-              </td>
-
+            <tr class="first" v-for="snapshot in snapshotsA" :id="snapshot.id">
+              <td style="display:none">{{snapshot.id}}</td>
+              <td>{{snapshot.name}}</td>
               <td class="description">
-                {{baseline.description}}
+                {{snapshot.description}}
               </td>
               <td>
                 <ul class="ulactions">
                   <li class="last">
-                    <input type="button" class="btn-flat primary" value="删除" @click="deleteBaseline($event)"/>
+                    <input type="button" class="btn-flat primary" value="删除"/>
                   </li>
 
                 </ul>
@@ -105,34 +75,28 @@
     </div>
 
 
-    <!--<hr/>
+    <hr/>
     <div>
-      {{baselines}}
-    </div>-->
+      {{snapshots}}
+    </div>
 
   </div>
 </template>
 
 <script>
   /* eslint-disable */
-  import Popper from 'vue-popperjs';
+
   export default {
-    components: {
-      'popper': Popper
-    },
     data() {
       return {
-        baselines: [],
-        searchQuery: '',
-        devname: '',
-        devip: '',
-        comps: ''   //此基线下所包含的组件
+        snapshots: [],
+        searchQuery: ''
       }
     }, created() {
       let projectId = this.getCookie('projectId');
       let username = this.getCookie('username');
       let password = this.getCookie('password');
-      this.$axios.get(this.getIP() + 'componentpackages', {
+      this.$axios.get(this.getIP() + '/deploymentdesignsnapshots', {
         //设置头
         headers: {
           'content-type': 'application/x-www-form-urlencoded'
@@ -142,121 +106,21 @@
           password: password
         }
       }).then(res => {
-        this.baselines = res.data.data
+        this.snapshots = res.data.data
       })
-      .catch(err => {
-        console.log(err);
-      })
+        .catch(err => {
+          console.log(err);
+        })
 
     },
     methods: {
-      deleteBaseline: function (event) {
-        //alert("A");
-        let e = event || window.event;
-        //alert("B");
-        let target = e.target || e.srcElement;
-
-        let msg = "您确定删除吗？";
-        if (confirm(msg) == true) {
-
-          if (target.parentNode.parentNode.parentNode.tagName.toLowerCase() == "td") {
-            //alert("C");
-            let rowIndex = target.parentNode.parentNode.parentNode.parentNode.rowIndex;
-            //alert(rowIndex);
-            let id = document.getElementById("table_value").rows[rowIndex].cells[0].innerHTML;
-            //alert(id);
-            let qs = require('qs');
-            this.$axios.delete(this.getIP() + 'componentpackages/' + id, {
-
-              //设置头
-              headers: {
-                'content-type': 'application/x-www-form-urlencoded'
-              },
-              auth: {
-                username: 'admin',
-                password: 'admin'
-              }
-            }).then(res => {
-              layer.msg("删除成功");
-
-              //删除完再次查询
-              let username = this.getCookie('username');
-              let password = this.getCookie('password');
-              this.$axios.get(this.getIP() + 'componentpackages', {
-                //设置头
-                headers: {
-                  'content-type': 'application/x-www-form-urlencoded'
-                },
-                auth: {
-                  username: username,
-                  password: password
-                }
-              }).then(res => {
-                this.baselines = res.data.data
-              })
-              .catch(err => {
-                console.log(err);
-              })
-
-            }).catch(err => {
-              layer.msg("删除失败！");
-            })
-          }
-
-        } else {
-          return false;
-        }
-
-
-      },
-
-      enter: function (event) {
-        let e = event || window.event;
-
-        let target = e.target || e.srcElement;
-
-        let compsInfo = target.parentNode.parentNode.parentNode;
-
-        let id = compsInfo.id;
-        console.log(compsInfo);
-        console.log(id);
-
-        let projectId = this.getCookie('projectId');
-        let username = this.getCookie('username');
-        let password = this.getCookie('password');
-
-        let qs = require('qs');
-        this.$axios.get(this.getIP() + 'componentpackages/' + id,{
-
-            //设置头
-            headers: {
-              'content-type': 'application/x-www-form-urlencoded'
-            },
-            auth: {
-              username: username,
-              password: password
-            }
-          }).then(res => {
-              this.comps = res.data.data.componentEntities
-
-          })
-          .catch(err => {
-
-            layer.msg("无信息！");
-          })
-      },
-
-      formReset: function () {
-        $("#input-path").val('');
-        $("#modal-path").modal('hide');
-      }
 
 
     },
     computed: {
-      baselinesA: function () {
+      snapshotsA: function () {
         let self = this;
-        return self.baselines.filter(function (item) {
+        return self.snapshots.filter(function (item) {
           return item.name.toLowerCase().indexOf(self.searchQuery.toLowerCase()) !== -1;
         })
 
@@ -265,12 +129,6 @@
   }
 </script>
 <style>
-  .tabletable {
-    height: 400px;
-    overflow-y: auto;
-    margin-top: -35px;
-  }
-
   .ulactions {
     margin: 5px 0 0 0;
   }
@@ -279,15 +137,4 @@
     display: inline;
   }
 
-  .addli {
-    height: 100px;
-    width: 200px;
-    max-height: 100px;
-    overflow: auto;
-  }
-
-  .addul {
-    text-align: left;
-    list-style:decimal;
-  }
 </style>
