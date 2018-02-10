cc1bffd1ede124d7d8906fae480456a2890ecf72
XYmar <xqingliuwu@163.com>
2018-02-10 17:38:42 +0800

    新版：基线页面：完成基线的删除功能

--- src/pages/baseline.vue
+++ src/pages/baseline.vue
@@ -45,7 +45,7 @@
               <td>
                 <ul class="ulactions">
                   <li class="last">
-                    <input type="button" class="btn-flat primary" value="删除"/>
+                    <input type="button" class="btn-flat primary" value="删除" @click="deleteBaseline($event)"/>
                   </li>
 
                 </ul>
@@ -114,6 +114,62 @@
 
     },
     methods: {
+      deleteBaseline: function (event) {
+        //alert("A");
+        let e = event || window.event;
+        //alert("B");
+        let target = e.target || e.srcElement;
+
+        let msg = "您确定删除吗？";
+        if (confirm(msg) == true) {
+
+          if (target.parentNode.parentNode.parentNode.tagName.toLowerCase() == "td") {
+            let rowIndex = target.parentNode.parentNode.parentNode.parentNode.rowIndex;
+            let id = document.getElementById("table_value").rows[rowIndex].cells[0].innerHTML;
+
+            this.$axios.delete(this.getIP() + 'deploymentdesignsnapshots/' + id, {
+
+              //设置头
+              headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+              },
+              auth: {
+                username: 'admin',
+                password: 'admin'
+              }
+            }).then(res => {
+              layer.msg("删除成功");
+
+              //删除完再次查询
+              let username = this.getCookie('username');
+              let password = this.getCookie('password');
+              this.$axios.get(this.getIP() + '/deploymentdesignsnapshots', {
+                //设置头
+                headers: {
+                  'content-type': 'application/x-www-form-urlencoded'
+                },
+                auth: {
+                  username: username,
+                  password: password
+                }
+              }).then(res => {
+                this.snapshots = res.data.data
+              })
+              .catch(err => {
+                console.log(err);
+              })
+
+            }).catch(err => {
+              layer.msg("删除失败！");
+            })
+          }
+
+        } else {
+          return false;
+        }
+
+
+      },
 
 
     },
