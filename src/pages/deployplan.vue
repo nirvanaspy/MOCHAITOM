46638ec21cb9feb6eff9e8a7a511286b5bf4d464
XYmar <xqingliuwu@163.com>
2018-02-22 14:12:58 +0800

    新版：部署设计页面：去掉下拉框

--- src/pages/deployplan.vue
+++ src/pages/deployplan.vue
@@ -8,50 +8,11 @@
                     <div class="span10 pull-right">
                         <input class="search" type="text" placeholder="部署设计名称.." v-model="searchQuery"/>
 
-                        <div class="ui-dropdown">
-                            <div class="head" data-toggle="tooltip" title="Click me!">
-                                Filter users
-                                <i class="arrow-down"></i>
-                            </div>
-                            <div class="dialog">
-                                <div class="pointer">
-                                    <div class="arrow"></div>
-                                    <div class="arrow_border"></div>
-                                </div>
-                                <div class="body">
-                                    <p class="title">
-                                        Show users where:
-                                    </p>
-                                    <div class="form">
-                                        <select>
-                                            <option />Name
-                                            <option />Email
-                                            <option />Number of orders
-                                            <option />Signed up
-                                            <option />Last seen
-                                        </select>
-                                        <select>
-                                            <option />is equal to
-                                            <option />is not equal to
-                                            <option />is greater than
-                                            <option />starts with
-                                            <option />contains
-                                        </select>
-                                        <input type="text" />
-                                        <a class="btn-flat small">Add filter</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
                         <router-link to="/addDeployPlan" class="btn-flat success pull-right">
                             <span>&#43;</span>
                             新增
                         </router-link>
-                        <!-- <router-link to="/xy1">
-                            <i class="icon-home"></i>
-                            <span>XY1</span>
-                        </router-link> -->
+
                     </div>
                 </div>
 
