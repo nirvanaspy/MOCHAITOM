c080a3303d6ac41d48d62b30de26d5faaa9bb9a4
XYmar <xqingliuwu@163.com>
2018-02-28 16:54:55 +0800

    新版：新增部署设计页面：新增部署设计的提示。

--- src/pages/addDeployPlan.vue
+++ src/pages/addDeployPlan.vue
@@ -82,7 +82,7 @@ let projectId = "2ec24245-0f8d-4db5-9d9b-1726ed727057";*/
                     this.$router.replace({ path: '/deployplan'})
                 })
                     .catch(err=>{
-                      layer.msg("请重新输入名称！");
+                      layer.msg("请输入名称！");
                     })
             },
 
